8d9718e005be2993b91807408e49d3f04b74637d
Josh Piedimonte <joshpied@gmail.com>
2020-01-23 20:28:00 -0500

    Added google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,10 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
-  /*
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' }
+  ],
+    /*
    ** Nuxt.js modules
    */
   modules: [],
